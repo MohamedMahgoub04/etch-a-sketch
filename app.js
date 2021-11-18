0f4debc4b72a25e114b0adef3aac34edddfc3039
Mohamed <M.Mahgoub04@gmail.com>
2021-11-19 01:12:46 +0400

    Add eraser

--- app.js
+++ app.js
@@ -4,34 +4,44 @@ const pen = main
 const background = '#e9ecef'
 
 function createGrid(side) {
+
  let blocks = side * side
  document.querySelector('#grid').style.gridTemplateRows = `repeat(${side}, auto)`
  document.querySelector('#grid').style.gridTemplateColumns = `repeat(${side}, auto)`
+
  for (let i = 1; i <= blocks; i++) {
   const div = document.createElement('div')
   div.className = `block block-${i}`
   document.querySelector('#grid').appendChild(div)
  }
+
  setBackground(background)
+
 }
 
 function setPen(color) {
+
  document.querySelectorAll('.block').forEach(x => {
   x.onmouseover = () => {
    x.style.backgroundColor = color
   } 
  })
+
 }
 
 function setBackground(color) {
+
   document.querySelectorAll('.block').forEach(x => {
     x.style.backgroundColor = color
   })
+
 }
 
 function setDefaultPen() {
+
  document.querySelector('#color').value = '#343a40'
  setPen(document.querySelector('#color').value)
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -56,6 +66,16 @@ document.addEventListener('DOMContentLoaded', () => {
   setDefaultPen()
  }
 
+ // Random
+ // Eraser
+ document.querySelector('#erase').onclick = () => {
+   document.querySelectorAll('.block').forEach(x => {
+     x.onmouseover = () => {
+       x.style.backgroundColor = background
+     }
+   })
+ }
+
  // Clear button
  document.querySelector('#clear').onclick = () => {
   document.querySelectorAll('.block').forEach(x => {
@@ -67,12 +87,10 @@ document.addEventListener('DOMContentLoaded', () => {
  document.querySelector('#change').onclick = () => {
   document.querySelector('#modal').style.display = 'flex'
   document.querySelector('form').onsubmit = () => {
-    console.log(document.querySelector('#size').value)
     
     document.querySelector('#grid').innerHTML = ''
     createGrid(document.querySelector('#size').value)
     setPen(main)
-    // selectColor(defaultColor)
     document.querySelector('#modal').style.display = 'none'
     document.querySelector('#size').value = ''
 
